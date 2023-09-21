3daa8723263a39d9d20d7ab0011f5a3d2617e1ff
ayushi.vaishnav <ayushi.vaishnav@gavstech.com>
2023-09-21 18:36:11 +0530

    Now we can access employees details.

---
+++ FirstWebAPI/Controllers/EmpController.cs
@@ -0,0 +1,30 @@
+﻿using FirstWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private RepositoryEmp _repositoryEmployee;
+        public EmpController(RepositoryEmp repository)
+        {
+            _repositoryEmployee = repository;
+        }
+        [HttpGet]
+        // GET: EmployeeController
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = _repositoryEmployee.GetAllEmp();
+            return employees;
+        }
+        [HttpPost]
+        public Employee EmployeeDetails(int id)
+        {
+            Employee employees = _repositoryEmployee.GetEmployeeId(id);
+            return employees;
+        }
+    }
+}
---
+++ FirstWebAPI/Models/RepositoryEmp.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstWebAPI.Models
+{
+    public class RepositoryEmp
+    {
+        private NorthwindContext _context;
+        public RepositoryEmp(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public List<Employee> GetAllEmp()
+        {
+
+            return _context.Employees.ToList();
+
+        }
+        public Employee GetEmployeeId(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            return employee;
+        }
+    }
+}
